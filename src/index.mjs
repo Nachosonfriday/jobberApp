1722ed14576047f69c1324ffc8fcabe57e04f2e6
Manuel Zuniga <manuelzuniga@Manuels-MacBook-Pro.local>
2025-08-20 21:23:28 -0600

    play with routes

--- src/index.mjs
+++ src/index.mjs
@@ -1,19 +1,39 @@
-// const express = require('express')
-// const app = express()
-// const port = 9000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Listening on ${port}`)
-// })
-
 import express from "express";
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+const mockUsers = [
+    { id: 1, username: "manuel", displayName: "Manuel"},
+    { id: 2, username: "olivia", displayName: "Olivia"},
+    { id: 3, username: "lucy", displayName: "Lucy"}
+  ];
+
+app.get('/', (req, res) => {
+  res.status(201).send('Hello World')
+});
+
+app.get('/api/users', (req, res) => {
+  res.send(mockUsers);
+});
+
+app.get('/api/users/:id', (req, res) => {
+  console.log(req.params);
+  const parsedID = parseInt(req.params.id);
+  console.log(parsedID);
+  if (isNaN(parsedID)) return res.status(400).send({ msg: "Bad Request"});
+
+  const findUser = mockUsers.find((user) => user.id === parsedID)
+  if (!findUser) return res.sendStatus(404);
+  return res.send(findUser);
+})
+
+app.get('/api/products', (req, res) => {
+  res.send([
+    { id: 123, name: 'chicken breast', price: 12.99}
+  ]);
+});
 
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
