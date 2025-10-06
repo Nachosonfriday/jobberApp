be23412b8891e10fd953081669cc5cf0e0c7d7d1
Manuel Zuniga <manuelzuniga@Manuels-MacBook-Pro.local>
2025-10-06 15:37:35 -0600

    add routes

---
+++ src/utils/constants.mjs
@@ -0,0 +1,9 @@
+export const mockUsers = [
+    { id: 1, username: "manuel", displayName: "Manuel"},
+    { id: 2, username: "olivia", displayName: "Olivia"},
+    { id: 3, username: "lucy", displayName: "Lucy"},
+    { id: 4, username: "kristen", displayName: "Kristen"},
+    { id: 5, username: "chris", displayName: "Chris"},
+    { id: 6, username: "jason", displayName: "Jason"},
+    { id: 7, username: "terry", displayName: "Terry"},
+  ];
